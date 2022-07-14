6bf85e517dc1f769cba2ca9ffb9aec029c8f2589
cpaschall <cpaschall216@gmail.com>
2022-07-14 13:30:37 -0400

    debugging server.js

--- server.js
+++ server.js
@@ -4,3 +4,39 @@ const mongodb = require('mongodb').MongoClient;
 const app = express();
 const port = 3001;
 
+const connectionStringURI = 'mongodb://127.0.0.1:27017/socialDB';
+
+let db = null;
+
+mongodb.connect(
+    connectionStringURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        db = client.db();
+        app.listen(port, () => {
+            console.log(`Listening on PORT ${port}`);
+        });
+    }
+);
+
+app.use(express.json());
+
+app.post('/create', (req, res) => {
+    db.collection('userCollection').insertOne(
+        { username: req.body.username, email: req.body.email },
+        (err, results) => {
+            if (err) throw err;
+            res.json(results)
+        }
+    );
+});
+
+app.get('/read', (req, res) => {
+    db.collection('userCollection')
+    .find()
+    .toArray((err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
