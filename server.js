07d604b0c2810dc0bdcc34eddab95f9e81851cf7
cpaschall <cpaschall216@gmail.com>
2022-07-14 21:49:35 -0400

    creating user model

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
+// const mongodb = require('mongodb').MongoClient;
+const db = require('./config/connection');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
-const port = 3001;
 
-const connectionStringURI = 'mongodb://127.0.0.1:27017/socialDB';
+const { User } = require('./models');
+// const port = 3001;
 
-let db = null;
+// const connectionStringURI = 'mongodb://127.0.0.1:27017/socialDB';
 
-mongodb.connect(
-    connectionStringURI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err, client) => {
-        db = client.db();
-        app.listen(port, () => {
-            console.log(`Listening on PORT ${port}`);
-        });
-    }
-);
+// let db = null;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// mongodb.connect(
+//     connectionStringURI,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     (err, client) => {
+//         db = client.db();
+//         app.listen(port, () => {
+//             console.log(`Listening on PORT ${port}`);
+//         });
+//     }
+// );
+
+// app.use(express.json());
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on PORt ${PORT}`)
+    });
+});
 
 app.post('/create', (req, res) => {
     db.collection('userCollection').insertOne(
